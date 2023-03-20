afa0c37ca6adecdec075eb759c13ae8cbf7a55ba
Lập trình cùng Eri <erichannel96@gmail.com>
2023-03-20 21:22:37 +0700

    update type casting

---
+++ 02_Data_type/type_casting_example/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 6;
+        System.out.println("i = " + i);
+        float j = i;
+        System.out.println("j = "+j);
+
+        float x = 6.5f;
+        System.out.println("x = " + x);
+        int y = (int) x;
+        System.out.println("y = " + y);
+    }
+}
