fbbc3a5c8a92e6227c152a92e7287bffe5ac6647
Lập trình cùng Eri <erichannel96@gmail.com>
2023-03-20 15:54:00 +0700

    update

---
+++ 03_Java_Operators/constant_example/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    final String DATABASE_NAME = "phone_book";
+    public static void main(String[] args) {
+        final double PI = 3.14;
+
+
+        //pi = 4.14;
+        double r = 2.5;
+        double s = PI * r * r;
+        System.out.println("Diện tích hình tròn: " + s);
+
+        String str = "Tôi tên là \"Eri\"";
+        System.out.println(str);
+    }
+}
